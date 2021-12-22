50ae71d178a358c8abfe339991450289a2418743
Konstantin Stepanov <688stakan@gmail.com>
2021-12-23 01:36:54 +0300

    Ninth task added

---
+++ 009_third_number/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Случайное число будет - ");
+int number = new Random().Next(-1000, 100000);
+Console.WriteLine(number);
+string numberA = number.ToString();
+if(numberA.Length < 2)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    char chr = numberA[2];
+    Console.WriteLine("третья цифра числа это: " + chr);
+}
