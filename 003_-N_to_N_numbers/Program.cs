d551ddd912bd5bd6d7a6d4717f5f8b746baa539b
Konstantin Stepanov <688stakan@gmail.com>
2021-12-22 22:51:24 +0300

    Third task added

---
+++ 003_-N_to_N_numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ??"0" );
+int i = 0;
+while( i <= N*2 )
+{
+    int number = -N + i;
+    Console.Write(number + " ");
+    i ++;
+}
