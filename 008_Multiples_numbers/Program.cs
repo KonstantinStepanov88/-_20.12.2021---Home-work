a6e7bb8851f6336ccde590db8e52e6a51e767d36
Konstantin Stepanov <688stakan@gmail.com>
2021-12-23 00:53:24 +0300

    Eighth task added

---
+++ 008_Multiples_numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = new Random().Next(1, 100000);
+Console.WriteLine("случайное число, которое будем делить - " + numberA);
+Console.WriteLine("Введите число на которое хотите делить:");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("задаваемый делитель - " + numberB);
+double result = numberA % numberB;
+
+if( result == 0)
+{
+    Console.WriteLine("Числа кратны");
+}
+else
+{
+    Console.WriteLine("Остаток от деления равен " + result);
+}
