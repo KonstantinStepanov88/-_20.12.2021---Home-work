8c5f0c854612a114e8e9b0d910a0aa29292507b8
Konstantin Stepanov <688stakan@gmail.com>
2021-12-22 23:49:32 +0300

    Sixth task added

---
+++ 006_Biggest_number/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+int first = number % 10;
+int second = (number / 10) % 10;
+if(first > second)
+{
+    Console.WriteLine("Наибольшая цифра из числа - " + first);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра из числа - " + second);
+}
