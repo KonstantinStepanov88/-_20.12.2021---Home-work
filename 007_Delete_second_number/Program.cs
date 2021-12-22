9b8c9660df14ec4bbc22f822adaad8dc0c620175
Konstantin Stepanov <688stakan@gmail.com>
2021-12-23 00:23:42 +0300

    Seventh task added

---
+++ 007_Delete_second_number/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine("вот такой номер был на входе - " + number);
+int first = (number / 100) % 10;
+int third = number % 10;
+Console.Write("а получился вот такой - ");
+Console.WriteLine(first.ToString() + third.ToString());
