867c3e6f2da9615af3d90e2e7b6dc3d546e3e976
Konstantin Stepanov <688stakan@gmail.com>
2021-12-22 23:26:22 +0300

    Fifth task added

---
+++ 005_Second_number/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int second = (number / 10) % 10;
+Console.WriteLine("Вторая цифра трёхзначного числа - " + second);
