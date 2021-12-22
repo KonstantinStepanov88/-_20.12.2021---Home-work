6f591edb15c33b7a1102bea81a99730e85abaed2
Konstantin Stepanov <688stakan@gmail.com>
2021-12-22 20:55:07 +0300

    Second task added

---
+++ 002_Week_days/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Значит сегодня - ");
+if( number == 1)
+{
+    Console.Write("понедельник");
+}
+if( number == 2)
+{
+    Console.Write("вторник");
+}
+if( number == 3)
+{
+    Console.Write("среда");
+}
+if( number == 4)
+{
+    Console.Write("четверг");
+}
+if( number == 5)
+{
+    Console.Write("пятница");
+}
+if( number == 6)
+{
+    Console.Write("суббота");
+}
+if( number == 7)
+{
+    Console.Write("воскресенье");
+}
