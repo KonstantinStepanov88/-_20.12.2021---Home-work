fbd6be80f6675ea675ba26bdb83d999d2ff2ead5
Konstantin Stepanov <688stakan@gmail.com>
2021-12-22 23:12:59 +0300

    Fourth task added

---
+++ 004_Last_Number/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int lastN = number % 10;
+Console.WriteLine("Последняя цифра трёхзначного числа - " + lastN);
