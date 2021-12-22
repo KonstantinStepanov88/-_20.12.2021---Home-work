d1619046b5ee903f451d96bd522851f0f43c3ca4
Konstantin Stepanov <688stakan@gmail.com>
2021-12-22 20:22:04 +0300

    First task added

---
+++ 001_First/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два числа: ");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+int numberB = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Является ли первое число квадратом второго? - ");
+if(numberA == numberB * numberB)
+{
+    Console.Write("True");
+}
+else
+{
+    Console.Write("False");
+}
