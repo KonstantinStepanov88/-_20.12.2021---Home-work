e48bbbaa9883b08b124d4795ce94cd390537421c
Konstantin Stepanov <688stakan@gmail.com>
2021-12-23 13:50:23 +0300

    Updated version of 009 Task added

---
+++ 009_Correct_New_version/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Случайное число будет - ");
+int number = new Random().Next(0, 100000000);
+Console.WriteLine(number);
+if(number >= 100)
+{
+    while(number >= 1000)
+    {
+        number = number / 10;
+    }
+    int third = number % 10;
+    Console.WriteLine("Третья цифра в номере: " + third);
+}
+else
+{
+    Console.WriteLine("В числе нет третьей цифры");
+}
